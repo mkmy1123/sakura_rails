04b53d7f040639834f291559ba909db8158c606b
satoshi ono <saongtx7@gmail.com>
2019-03-17 13:00:47 +0900

    engin.rb

--- lib/sakura_rails/engine.rb
+++ lib/sakura_rails/engine.rb
@@ -1,5 +1,19 @@
 module SakuraRails
   class Engine < ::Rails::Engine
+   
+  end
+end
+
+
+
+require 'sakura_rails/view_helper'
+module SakuraRails
+  class Engine < ::Rails::Engine
     isolate_namespace SakuraRails
+    initializer 'good_item.action_view_helpers' do
+      ActiveSupport.on_load :action_view do
+        include SakuraRails::ViewHelper
+      end
+    end
   end
 end
--- lib/sakura_rails/version.rb
+++ lib/sakura_rails/version.rb
@@ -1,3 +1,3 @@
 module SakuraRails
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
